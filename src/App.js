be1c7b644d102273e50ff8eb3dbb41750c96fd7a
andreawatkins <awatki22@depaul.edu>
2022-04-27 11:54:22 -0500

    setting up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import React from 'react';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1> Andrea is building a Netflix Clone </h1>
-      
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title = "Trending Now" />
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Row({title}) {
+    const [movies, setMovies] = useState([]);
+    
+  return (
+    <div>
+        <h2>{title}</h2>
+       
+    </div>
+  )
+}
+
+export default Row
